6e58f599a5496130232c8231179df3a17516b25e
Finlay <fehorsburgh@gmail.com>
2026-06-12 22:51:52 +0100

    Add exit builtin and escape character skipping

--- src/execution.c
+++ src/execution.c
@@ -3,34 +3,43 @@
 #include <sys/wait.h>
 
 int execTokens(TokenList list) {
-    char **args = malloc(sizeof(char *));
+        char **args = malloc(sizeof(char *));
 
-    size_t arg_count = 0;
-    for (size_t i = 0; i < list.size; ++i) {
-        // Currently ignore non-IDENTIFIER tokens
-        // keep getting tokens as arguments if token.tag == IDENTIFIER
-        if (list.tokens[i].tag == IDENTIFIER) {
-            args = realloc(args, (arg_count + 1) * sizeof(char *));
-            args[arg_count] = list.tokens[i].value;
-            ++arg_count;
+        size_t arg_count = 0;
+        for (size_t i = 0; i < list.size; ++i) {
+                // Currently ignore non-IDENTIFIER tokens
+                // keep getting tokens as arguments if token.tag == IDENTIFIER
+                if (list.tokens[i].tag == IDENTIFIER) {
+                        args = realloc(args, (arg_count + 1) * sizeof(char *));
+                        args[arg_count] = list.tokens[i].value;
+                        ++arg_count;
+                }
         }
-    }
 
-    fflush(NULL);
-    pid_t p = fork();
-    if (p < 0) {
-        perror("fork");
+        if (!strcmp(args[0], "exit")) {
+                int exitcode = atoi(args[1]);
+                exit(exitcode);
+        }
+
+        fflush(NULL);
+        pid_t p = fork();
+        if (p < 0) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+        } else if (!p) {
+
+                printf("args: \n");
+                for (size_t i = 0; i < arg_count; ++i) {
+                        printf("  %s\n", args[i]);
+                }
+                execvp(args[0], args);
+
+        } else {
+                int status = 0;
+                wait(&status);
+                int exit_status = WEXITSTATUS(status);
+                return exit_status;
+        }
+
         exit(EXIT_FAILURE);
-    } else if (!p) {
-        printf("executing command: %s\n", args[0]);
-        execvp(args[0], args);
-
-    } else {
-        int status = 0;
-        wait(&status);
-        int exit_status = WEXITSTATUS(status);
-        return exit_status;
-    }
-
-    exit(EXIT_FAILURE);
 }
--- src/tokenize.c
+++ src/tokenize.c
@@ -3,100 +3,104 @@
 #include <ctype.h>
 
 char *getTagString(Tag tag) {
-    switch (tag) {
-    case UNKNOWN:
-        return "UNKNOWN";
-    case IDENTIFIER:
-        return "IDENTIFIER";
-    case END:
-        return "END";
-    default:
-        perror("Invalid tag");
-        exit(EXIT_FAILURE);
-    }
+        switch (tag) {
+        case UNKNOWN:
+                return "UNKNOWN";
+        case IDENTIFIER:
+                return "IDENTIFIER";
+        case END:
+                return "END";
+        default:
+                perror("Invalid tag");
+                exit(EXIT_FAILURE);
+        }
 }
 
 void printTokens(TokenList list) {
-    for (size_t i = 0; i < list.size; ++i) {
-        printf("#%zu: ", i);
-        printf("tag: %s ", getTagString(list.tokens[i].tag));
-        printf("value: %s\n", list.tokens[i].value);
-    }
+        for (size_t i = 0; i < list.size; ++i) {
+                printf("#%zu: ", i);
+                printf("tag: %s ", getTagString(list.tokens[i].tag));
+                printf("value: %s\n", list.tokens[i].value);
+        }
 }
 
 void skipSpace(char **line, size_t linesize) {
-    size_t pos = 0;
-    while (pos < linesize) {
-        if ((*line)[pos] == ' ' || (*line)[pos] == '\n') {
-            (*line)[pos] = '\0';
-            ++pos;
-        } else {
-            break;
+        size_t pos = 0;
+        while (pos < linesize) {
+                if ((*line)[pos] == ' ' || (*line)[pos] == '\n') {
+                        (*line)[pos] = '\0';
+                        ++pos;
+                } else {
+                        break;
+                }
         }
-    }
-    *line += pos;
+        *line += pos;
 }
 
 Token getTokenAlphanum(char **line) {
-    size_t sliceto = 1;
-    while ((*line)[sliceto] && isalnum((*line)[sliceto])) {
-        ++sliceto;
-    }
-    Token token = {};
-    token.tag = IDENTIFIER;
-    token.value = *line;
-    token.valsize = sliceto;
-    *line += sliceto;
-    return token;
+        size_t sliceto = 1;
+        while ((*line)[sliceto]) {
+                if (isalnum((*line)[sliceto])) {
+                        ++sliceto;
+                } else if ((*line)[sliceto] == '\\') {
+                        sliceto += 2;
+                } else {
+                        break;
+                }
+        }
+        Token token = {};
+        token.tag = IDENTIFIER;
+        token.value = *line;
+        token.valsize = sliceto;
+        *line += sliceto;
+        return token;
 }
 
 Token getToken(char **line) {
-    size_t pos = 0;
-    char first = (*line)[pos];
-    if (isalpha(first) || first == '_') {
-        return getTokenAlphanum(line);
-    } else if (first == '\\') {
-        fprintf(stderr, "escape characters not yet implemented");
-        exit(1);
-    } else {
-        *line += (pos + 1);
-        Token token;
-        if (!first) {
+        size_t pos = 0;
+        char first = (*line)[pos];
+        if (isalnum(first) || first == '_' || first == '\\') {
+                return getTokenAlphanum(line);
 
-            token.tag = END;
         } else {
-            token.tag = UNKNOWN;
+                *line += (pos + 1);
+                Token token;
+                if (!first) {
+
+                        token.tag = END;
+                } else {
+                        token.tag = UNKNOWN;
+                }
+                token.value = &first;
+                return token;
         }
-        token.value = &first;
-        return token;
-    }
 }
 
 TokenList tokenizeLine(char *line) {
-    TokenList list = {};
-    if (!line) {
-        list.tokens = nullptr;
-        list.size = 0;
-        return list;
-    }
-    Token *tokens = (Token *)malloc(sizeof(Token));
-    size_t allocatedsize = 0;
+        TokenList list = {};
+        if (!line) {
+                list.tokens = nullptr;
+                list.size = 0;
+                return list;
+        }
+        Token *tokens = (Token *)malloc(sizeof(Token));
+        size_t allocatedsize = 0;
 
-    do {
-        skipSpace(&line, strlen(line));
-        Token token = getToken(&line);
-        if (token.tag == END) {
-            break;
-        }
-        tokens = realloc(tokens, (allocatedsize + 1) * sizeof(Token));
-        if (!tokens) {
-            perror("realloc");
-            exit(EXIT_FAILURE);
-        }
-        tokens[allocatedsize] = token;
-        ++allocatedsize;
-    } while (*line);
-    list.size = allocatedsize;
-    list.tokens = tokens;
-    return list;
+        do {
+                skipSpace(&line, strlen(line));
+                Token token = getToken(&line);
+                if (token.tag == END) {
+                        break;
+                }
+                tokens = realloc(tokens, (allocatedsize + 1) * sizeof(Token));
+                if (!tokens) {
+                        perror("realloc");
+                        exit(EXIT_FAILURE);
+                }
+                tokens[allocatedsize] = token;
+                ++allocatedsize;
+        } while (*line);
+        list.size = allocatedsize;
+        list.tokens = tokens;
+        return list;
 }
